688c7394e41e59b0fcb738dfcf43f955a985f5d9
Kyle Schmitz <kyleschmitz@outlook.com>
2018-10-17 17:00:15 -0500

    Adding class 10 notes)

---
+++ Class10/README.md
@@ -0,0 +1,17 @@
+# Class 10 - Oct 17, 2018
+
+## Agenda
+
+* Finish learning about Arrays and Matricies.
+  * https://drive.google.com/open?id=1NTchJS7TClpx16kzNrveWlmXHhmVYGpl5PDq1we_xyc
+* Learn about Types and Conversions.
+  * https://drive.google.com/open?id=10eU1ALWhvJnrJ7_9pK_dNh6qgu7pYt2K5RmSdZCdVvQ
+* Examples from class:
+  * (Not posted yet)
+
+Next class will be learning about **Object Oriented Programming**
+
+## Homework
+
+1. Complete the following assignment in C#:
+  * https://drive.google.com/open?id=1wDiq84RUiBnEY_MEb0UOr67BxdI2rWD0s-HqFWAROQI
---
+++ Class10/Rocket/Rocket/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Rocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rocketHasCrashed = false;
+
+            while (!rocketHasCrashed)
+            {
+                Console.WriteLine();
+                Console.WriteLine("***");
+                Console.WriteLine();
+
+                int speedInMetersPerSecond = GetTheSpeedOfTheRocket();
+                double neededFuel = CalculateNeededFuel((short)speedInMetersPerSecond);
+
+                Console.WriteLine($"Sending {neededFuel} gallons of fuel to the engine to maintain flight!");
+
+                if (neededFuel < 0)
+                {
+                    rocketHasCrashed = true;
+                }
+            }
+
+            Console.WriteLine("Oh no! The rocket has crashed! You better start looking for a new job...");
+            Console.ReadKey();
+        }
+
+        static int GetTheSpeedOfTheRocket()
+        {
+            int speedInMetersPerSecond;
+
+            Console.Write("Please enter the speed of the rocket in meters per second: ");
+            speedInMetersPerSecond = Convert.ToInt32(Console.ReadLine());
+
+            return speedInMetersPerSecond;
+        }
+
+        static double CalculateNeededFuel(short speedInMetersPerSecond)
+        {
+            const double FUEL_RATIO = 0.2;
+            double neededFuelInGallons = speedInMetersPerSecond * FUEL_RATIO;
+
+            Console.WriteLine($"Calculating needed fuel for a speed of {speedInMetersPerSecond} meters per second.");
+            Console.WriteLine($"Needed fuel in gallons is: {neededFuelInGallons}");
+
+            return neededFuelInGallons;
+        }
+    }
+}
