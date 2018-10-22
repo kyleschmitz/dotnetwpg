9c12a884ee46a050426d762aaa6d7912e7b7d4c9
Kyle Schmitz <kyleschmitz@outlook.com>
2018-10-22 17:17:37 -0500

    Adding class 10 example

---
+++ Class11/BookExample/BookExample/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BookExample
+{
+    class Book
+    {
+        public string Name;
+        public int pages;
+
+        public void PrintBookInformation()
+        {
+            Console.WriteLine($"Book Title: {Name}, number of pages: {pages}");
+        }
+    }
+
+
+}
---
+++ Class11/BookExample/BookExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BookExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = GetBooks();
+            Book largestBook = books[0];
+
+            for (int i = 1; i < books.Length; i++)
+            {
+                if (books[i].pages > largestBook.pages)
+                {
+                    largestBook = books[i];
+                }
+            }
+
+            Console.Write($"The largest book is: ");
+            largestBook.PrintBookInformation();
+
+            Console.ReadKey();
+        }
+
+        static Book[] GetBooks()
+        {
+            Console.Write("Enter Number of books: ");
+            int numberOfBooks = Convert.ToInt32(Console.ReadLine());
+            Book[] books = new Book[numberOfBooks];
+
+            for (int i = 0; i < numberOfBooks; i++)
+            {
+                books[i] = new Book();
+
+                Console.Write($"Enter name of book {i + 1}: ");
+                books[i].Name = Console.ReadLine();
+                
+                Console.Write($"Enter number of pages for book {i + 1}: ");
+                books[i].pages = Convert.ToInt32(Console.ReadLine());
+                
+            }
+
+            return books;
+        }
+    }
+}
